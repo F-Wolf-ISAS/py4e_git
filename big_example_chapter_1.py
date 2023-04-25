7f53542755aa526eea2ef6dec26780a2b9933b4f
F-Wolf-ISAS <felix.wolf@gmx.com>
2023-04-25 09:51:33 +0200

    new file

---
+++ big_example_chapter_1.py
@@ -0,0 +1,20 @@
+# Python-Code to make a histogram about the occurence of words in a text.file.
+# In the end, it prints out the word together with the number of occurences.
+
+name = input('Enter file:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
